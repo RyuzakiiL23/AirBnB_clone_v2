6edd4cf521afe69fceeeefe9358e26d4478999de
kerzers <ahra.ahlame@gmail.com>
2023-09-19 03:56:30 -0700

    task 8

--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -12,3 +14,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
