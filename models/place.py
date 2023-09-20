2be5baea78ab64b19fc1950dd03c6fef08c73a3f
kerzers <ahra.ahlame@gmail.com>
2023-09-20 05:55:22 -0700

    task 10

--- models/place.py
+++ models/place.py
@@ -2,10 +2,20 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
-from models.review import Review
 from os import getenv
+import models
+
+
+place_amenity = Table(
+             'place_amenity',
+             Base.metadata,
+             Column('place_id', String(60),
+                    ForeignKey('places.id'), nullable=False),
+             Column('amenity_id', String(60),
+                    ForeignKey('amenities.id'), nullable=False)
+             )
 
 
 class Place(BaseModel, Base):
@@ -26,11 +36,26 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates='place_amenities')
 
     @property
     def reviews(self):
+        """ getter attribute of reviews"""
         rev_list = []
         for review in reviews:
             if review.place_id == self.id:
                 rev_list.append(review)
         return rev_list
+
+    @property
+    def amenities(self):
+        """ getter/setter for amenities"""
+        return self.amenity_ids
+
+    @amenities.setter
+    def amenities(self, obj=None):
+        """setter of amenities"""
+        if isinstance(obj, Amenity) and obj.id not in self.amenity_ids:
+            self.amenity_ids.append(obj.id)
