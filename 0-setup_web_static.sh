9149607264d6b443e4c4d77bb0b30b5b0288dfd4
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-04 20:27:30 +0100

    task_0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Configurating nginx server
+
+sudo apt-get update
+sudo apt install nginx -y
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Hello server" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+}" > /etc/nginx/sites-available/default
+
+sudo service nginx restart
